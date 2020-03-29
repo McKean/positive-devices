f95dfe383183dc19b998e57cd5cd1cc4b3efa319
berguera <robin.berguerand@epfl.ch>
2020-03-29 09:48:12 +0200

    removed webcopy, some inital code

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const slsw = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+//const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: slsw.lib.entries,
