182419ef62e3b685332b806cd6d87346433452fa
Christopher Scott <christopher.scott@cmcks.com>
2020-03-29 14:32:20 +0200

    changed adding to work for local dynamo

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -6,11 +6,21 @@ jest.setTimeout(500000);
 
 const lambdaWrapper = jestPlugin.lambdaWrapper;
 const wrapped = lambdaWrapper.wrap(mod, {
-  handler: 'handler'
+  handler: 'add'
 });
 
 describe('add', () => {
   beforeEach(() => {});
+
+  // @ts-ignore because createEvent will fill the blanks
+  const event = createEvent('aws:apiGateway', {
+    body: '{ "id": "hello", "seen": "world" }',
+    httpMethod: 'POST',
+    // @ts-ignore because createEvent will fill the blanks
+    requestContext: {
+      httpMethod: 'POST'
+    }
+  });
 
   it('should be able to provide relationship data', async () => {
     return wrapped.run(event).then((response: any) => {
--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,63 @@
-import { DynamoDB, SQS, SNS } from 'aws-sdk';
-const dynamoDb = new DynamoDB.DocumentClient();
-const millisecperday=24*3600*1000;
+import { DynamoDB, SQS, SNS, APIGateway } from 'aws-sdk';
+import { APIGatewayProxyEvent } from 'aws-lambda';
 
-export const add = async (event: any) => {
-  const { ida, idb } = event.queryStringParameters;
+const dynamoDb = new DynamoDB.DocumentClient({
+  region: 'localhost',
+  endpoint: 'http://localhost:8000',
+  accessKeyId: 'DEFAULT_ACCESS_KEY', // needed if you don't have aws credentials at all in env
+  secretAccessKey: 'DEFAULT_SECRET' // needed if you don't have aws credentials at all in env
+});
+const millisecperday = 24 * 3600 * 1000;
+
+interface AddPost {
+  id: string;
+  seen: string;
+}
+
+export const add = async (event: APIGatewayProxyEvent) => {
+  const body: string = event.body || '';
+  const { id, seen } = JSON.parse(body) as AddPost;
   const timestamp = new Date().getTime();
 
   const params = {
-      TableName: "Entry",
-      Item: {
-        EntryType:"Rel",
-        id1:ida,
-        id2:idb,
-        Timestamp:timestamp,
-      },
+    TableName: 'Entry',
+    Item: {
+      EntryType: 'Rel',
+      Id: id,
+      Seen: seen,
+      T: timestamp
+    }
   };
 
   dynamoDb.put(params, (error, result) => {
-      if (error) {
-        console.error(error)
-        return
-      }
-      return
-    });
-  classify_1(ida,idb);
+    if (error) {
+      console.error(error);
+      return;
+    }
+    console.log(result);
+    return;
+  });
+  classify_1(ida, idb);
 };
 
-
-function classify_1(id1:string, id2:string) {
-  const timestamp = new Date().getTime()
-  const params= {
-  TableName : "Entry",
-  KeyConditionExpression: "(NOT (EntryType=:rel || EntryType=:lvl3)) AND id1=:y AND Timestamp>:num",
-  ExpressionAttributeValues: {
-       ":rel": "rel",
-       ":lvl3": "l-3",
-       ":y":id1,
-       ":num": timestamp-20*millisecperday,
-  },
-}
-    dynamoDb.query(params, function(err, data) {
-        //if (err) console.log(err);
-        //else console.log(data);
-        return
-    })
+function classify_1(id1: string, id2: string) {
+  const timestamp = new Date().getTime();
+  const params = {
+    TableName: 'Entry',
+    KeyConditionExpression:
+      '(NOT (EntryType=:rel || EntryType=:lvl3)) AND id1=:y AND Timestamp>:num',
+    ExpressionAttributeValues: {
+      ':rel': 'rel',
+      ':lvl3': 'l-3',
+      ':y': id1,
+      ':num': timestamp - 20 * millisecperday
+    }
+  };
+  dynamoDb.query(params, function(err, data) {
+    //if (err) console.log(err);
+    //else console.log(data);
+    return;
+  });
 }
 export const report = async (event: any) => {
   const { id } = event.queryStringParameters;
