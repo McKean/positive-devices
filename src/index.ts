971bb199cb5895b3786f491c21f6e2d4bce8b4c9
berguera <robin.berguerand@epfl.ch>
2020-03-29 09:48:52 +0200

    removed webcopy, some inital code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { DynamoDB, SQS, SNS } from 'aws-sdk';
 const dynamoDb = new DynamoDB.DocumentClient();
 const millisecperday=24*3600*1000;
-export const add = async (event: any) => {
 
+export const add = async (event: any) => {
   const { ida, idb } = event.queryStringParameters;
   const timestamp = new Date().getTime();
+
   const params = {
       TableName: "Entry",
       Item: {
@@ -14,7 +15,7 @@ export const add = async (event: any) => {
         Timestamp:timestamp,
       },
   };
-  console.log("yolo");
+
   dynamoDb.put(params, (error, result) => {
       if (error) {
         console.error(error)
@@ -22,28 +23,27 @@ export const add = async (event: any) => {
       }
       return
     });
-    console.log("yolo");
   classify_1(ida,idb);
 };
-function classify_1(id1:string, id2:string) {
-      const timestamp = new Date().getTime()
-      const params= {
-      TableName : "Entry",
-      KeyConditionExpression: "(NOT (EntryType=:rel || EntryType=:lvl3)) AND id1=:y AND Timestamp>:num",
-      ExpressionAttributeValues: {
-           ":rel": "rel",
-           ":lvl3": "l-3",
-           ":y":id1,
-           ":num": timestamp-20*millisecperday,
-      },
-    }
-  console.log("aits");
-    dynamoDb.query(params, function(err, data) {
 
+
+function classify_1(id1:string, id2:string) {
+  const timestamp = new Date().getTime()
+  const params= {
+  TableName : "Entry",
+  KeyConditionExpression: "(NOT (EntryType=:rel || EntryType=:lvl3)) AND id1=:y AND Timestamp>:num",
+  ExpressionAttributeValues: {
+       ":rel": "rel",
+       ":lvl3": "l-3",
+       ":y":id1,
+       ":num": timestamp-20*millisecperday,
+  },
+}
+    dynamoDb.query(params, function(err, data) {
         //if (err) console.log(err);
         //else console.log(data);
         return
-      })
+    })
 }
 export const report = async (event: any) => {
   const { id } = event.queryStringParameters;
